d4db053db9ef8a0421d200da704ffaa298488b1f
pedroleaop <pedroleaop@hotmail.com>
2021-03-13 16:24:34 -0300

    menu

--- src/components/MenuTop.vue
+++ src/components/MenuTop.vue
@@ -3,8 +3,14 @@
     <v-app-bar color="green darken-2" dense dark>
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Onde tem coleta?</v-toolbar-title>
+      <v-toolbar-title><b>Onde tem coleta?</b></v-toolbar-title>
+      <div id="switchbtn"><v-switch
+        v-model="$vuetify.theme.dark"
+        persistent-hint
+      ></v-switch></div>
+
     </v-app-bar>
+
     <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list nav dense>
         <v-list-item-group v-model="group" active-class="blue text--lighten-2">
@@ -20,29 +26,42 @@
           <router-link to="/pontosColeta">
             <v-list-item>
               <v-list-item-icon>
-                <v-icon></v-icon>
+                <v-icon>mdi-place</v-icon>
               </v-list-item-icon>
               <v-list-item-title> Pontos de coleta</v-list-item-title>
             </v-list-item>
           </router-link>
 
+
+ <router-link to="/como-fazer">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-help_center</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title> Como separar o seu lixo</v-list-item-title>
+            </v-list-item>
+          </router-link>
+
+
+
           <router-link to="/novo">
             <v-list-item>
               <v-list-item-icon>
-                <v-icon></v-icon>
+               <v-icon>mdi-home-plus-outline</v-icon>
               </v-list-item-icon>
               <v-list-item-title> Adicionar ponto de coleta</v-list-item-title>
             </v-list-item>
           </router-link>
 
-          <router-link to="/como-fazer">
+       <router-link to="/novo-coletor">
             <v-list-item>
               <v-list-item-icon>
-                <v-icon></v-icon>
+                <v-icon>mdi-account-plus-outline</v-icon>
               </v-list-item-icon>
-              <v-list-item-title> Como fazer a coleta</v-list-item-title>
+              <v-list-item-title> Adicionar catador</v-list-item-title>
             </v-list-item>
           </router-link>
+          <br><br>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
@@ -62,5 +81,12 @@ export default {
 <style scoped>
 .app-top-bar a {
   text-decoration: none !important;
+}
+
+#switchbtn {
+  display: flexbox;
+  position: absolute;
+  top: 10px;
+  right: 10px;
 }
 </style>
